94e6b041f98471a76fce26fdba4de211dfbd7645
Gabriel <moraisgabriel16@gmail.com>
2024-11-13 11:23:54 -0300

    Fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,39 @@
 // src/components/Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // Novo arquivo CSS para melhorar os estilos do Navbar
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
                 <Link className="navbar-brand" to="/imobiliaria">Imobiliária</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    onClick={toggleMenu}
+                    aria-controls="navbarNav"
+                    aria-expanded={isOpen ? "true" : "false"}
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/clientes">Clientes</Link>
+                            <Link className="nav-link" to="/clientes" onClick={() => setIsOpen(false)}>Clientes</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/cadastro-cliente">Cadastrar Cliente</Link>
+                            <Link className="nav-link" to="/cadastro-cliente" onClick={() => setIsOpen(false)}>Cadastrar Cliente</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">Buscar Cliente</Link>
+                            <Link className="nav-link" to="/home" onClick={() => setIsOpen(false)}>Buscar Cliente</Link>
                         </li>
                     </ul>
                 </div>
